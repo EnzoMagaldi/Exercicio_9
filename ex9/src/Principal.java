6ffcae2619e1c53e965891dbb16a8425fbf04226
EnzoMagaldi <166729058+EnzoMagaldi@users.noreply.github.com>
2025-01-14 13:14:39 -0300

    Update Principal.java

--- ex9/src/Principal.java
+++ ex9/src/Principal.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Principal {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
         
         String [] autores = {"Eric Freeman", "Elisabeth Freeman"};
@@ -12,12 +13,9 @@ public class Principal {
         // Criar publicações
         Publicacao headFirst = new Livro("Padrões de Projeto", "Programação", "2a", oreilly, autores);
         Publicacao oracle = new Revista("Isto É", "Notícias", "2279", tres, "semanal");
+        //Publicacao headFirst2 = new Livro("Padrões de Projeto", "Programação", "3a", oreilly, autores);
         Publicacao dev = new Livro("HTML5 P.E.","Programação", "5a",oreilly,autores2);
-
-        // Adicionar publicações às editoras
-        oreilly.addPub(headFirst);
-        oreilly.addPub(dev);
-        tres.addPub(oracle);
+        //Publicacao oracle2 = new Revista("Isto É", "Notícias", "2278", tres, "semanal");
 
         // Salvar editoras e publicações no arquivo
         List<Editora> editoras = new ArrayList<>();
